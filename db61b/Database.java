d35611bbf3f45f86aba0d00dbde1afd1d059df18
dyx0718 <dyx0718@gmail.com>
2015-10-26 16:58:51 -0700

    done

---
+++ db61b/Database.java
@@ -0,0 +1,30 @@
+
+package db61b;
+import java.util.HashMap;
+
+/** A collection of Tables, indexed by name.
+ *  @author Yixin Du*/
+class Database {
+    /** An empty database. */
+    public Database() {
+        _tables = new HashMap<String, Table>();
+    }
+
+    /** Return the Table whose name is NAME stored in this database, or null
+     *  if there is no such table. */
+    public Table get(String name) {
+        return _tables.get(name);
+    }
+
+    /** Set or replace the table named NAME in THIS to TABLE.  TABLE and
+     *  NAME must not be null, and NAME must be a valid name for a table. */
+    public void put(String name, Table table) {
+        if (name == null || table == null) {
+            throw new IllegalArgumentException("null argument");
+        }
+        _tables.put(name, table);
+    }
+    /** A table set. */
+    private HashMap<String, Table> _tables;
+
+}
